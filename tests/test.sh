b59d12f0374f5cb215ba75fd84fcf1d07b16084e
Hrafn Orri Hrafnkelsson <HrafnOrri@BitCrow.net>
2022-10-22 21:53:29 +0000

    test

--- tests/test.sh
+++ tests/test.sh
@@ -1,11 +1,13 @@
 #!/bin/ash
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 # csharp
-(cd ./cs/error && otter && exit 1)
-(cd ./cs/ok && otter || exit 1)
+(cd ${SCRIPT_DIR}/cs/error && otter && exit 1)
+(cd ${SCRIPT_DIR}/cs/ok && otter || exit 1)
 
 # golang
-(cd ./go/error && otter && exit 1)
-(cd ./go/ok && otter || exit 1)
+(cd ${SCRIPT_DIR}/go/error && otter && exit 1)
+(cd ${SCRIPT_DIR}/go/ok && otter || exit 1)
 
 exit 0
