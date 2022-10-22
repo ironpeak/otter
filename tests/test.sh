494609d6548b85ad9ca1ead5ce9d1ac4c182e2d2
Hrafn Orri Hrafnkelsson <HrafnOrri@BitCrow.net>
2022-10-22 23:47:43 +0000

    update

--- tests/test.sh
+++ tests/test.sh
@@ -3,11 +3,11 @@
 BASEDIR=$(dirname "$0")
 
 # csharp
-(cd ${BASEDIR}/cs/error && otter && exit 1)
-(cd ${BASEDIR}/cs/ok && otter || exit 1)
+cd ${BASEDIR}/cs/error && otter && exit 1
+cd ${BASEDIR}/cs/ok && otter || exit 1
 
 # golang
-(cd ${BASEDIR}/go/error && otter && exit 1)
-(cd ${BASEDIR}/go/ok && otter || exit 1)
+cd ${BASEDIR}/go/error && otter && exit 1
+cd ${BASEDIR}/go/ok && otter || exit 1
 
 exit 0
