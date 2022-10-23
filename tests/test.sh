faa9b343d312aa53568b2c8e9ad11e79b50660a7
Hrafn Orri Hrafnkelsson <HrafnOrri@BitCrow.net>
2022-10-23 00:09:10 +0000

    parenthesise

--- tests/test.sh
+++ tests/test.sh
@@ -5,11 +5,11 @@ BASEDIR=$(dirname "$0")
 echo "Running Tests"
 
 # csharp
-cd ${BASEDIR}/cs/error && otter && exit 1
-cd ${BASEDIR}/cs/ok && otter || exit 1
+(cd ${BASEDIR}/cs/error && otter) && exit 1
+(cd ${BASEDIR}/cs/ok && otter) || exit 1
 
 # golang
-cd ${BASEDIR}/go/error && otter && exit 1
-cd ${BASEDIR}/go/ok && otter || exit 1
+(cd ${BASEDIR}/go/error && otter) && exit 1
+(cd ${BASEDIR}/go/ok && otter) || exit 1
 
 exit 0
